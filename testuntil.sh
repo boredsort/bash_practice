1db43531e126874363c80396082189394fa918c1
kenntoot <lefthandmastery@gmail.com>
2023-05-01 21:39:57 +0800

    Added loop examples

---
+++ testuntil.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+n=$1
+[ "$n" == '' ] && echo please give a number and type and try again && exit
+
+factorial=1 ; j=1
+
+until [ $j -le "$n" ]
+do
+    factorial=$(( factorial * j ))
+    j=$((j+1))
+done
+echo The factorial of n, "$n"'!' = $factorial
+exit 0
